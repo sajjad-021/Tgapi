2b78e204acface0b1a17c7d0dd1ab22e4ea56d25
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-09-05 16:55:37 -0700

    Update api.lua

--- api.lua
+++ api.lua
@@ -159,7 +159,7 @@ local function run()
           offset = msg.update_id + 1
           if msg.inline_query then
             local q = msg.inline_query
-						if q.from.id == 391052362 or q.from.id == 66488544 or q.from.id == 180191663 then
+						if q.from.id == 400674938 or q.from.id == 66488544 then
             if q.query:match('%d+') then
               local chat = '-'..q.query:match('%d+')
 							local function is_lock(chat,value)
